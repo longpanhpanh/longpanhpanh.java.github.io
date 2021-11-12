42a0492141aade28cd9b71048f3e65da909f8948
longpanhpanh <tranthanhlongita@gmail.com>
2021-11-12 17:14:50 +0700

    Code Music

--- Lesson/Maven/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Main {
-    public static void main(String[] args) {
-        MovieRepository repo = new MovieRepository();
-        ArrayList<Movie> list = repo.getData();
-        repo.printList(list);
-    }
-}
