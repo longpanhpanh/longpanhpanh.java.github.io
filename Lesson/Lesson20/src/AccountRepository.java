6346732273875ffad38e36898e64d8863f9adfe1
longpanhpanh <tranthanhlongita@gmail.com>
2021-12-05 19:09:39 +0700

    sửa Lesson 20

--- Lesson/Lesson20/src/AccountRepository.java
+++ Lesson/Lesson20/src/AccountRepository.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AccountRepository {
+
+
+
     Scanner sc = new Scanner(System.in);
-    ArrayList<Account> listAccount;
+    ArrayList<Account> listAccount = getAccount();
+    boolean isCheck = false;
     public ArrayList<Account> getAccount() {
         listAccount = new ArrayList<>();
         listAccount.add(new Account("tranthanhlong", "Tranthanhlong123_", "tranthanhlongita@gmail.com"));
@@ -14,165 +19,187 @@ public class AccountRepository {
     }
 
 
+
+
     public void print(ArrayList<Account> list) {
         list.forEach(System.out::println);
     }
 
-
-
-    public String checkUsername () {
+    public void checkLogin ()  {
+        System.out.println("Nhập username");
         String user = sc.nextLine();
-        if (!listAccount.stream().anyMatch(a -> a.getUsername().equals(user))) {
+        if (listAccount.stream().anyMatch(account -> account.getUsername().equals(user))) {
+            Account acc = listAccount.stream()
+                    .filter(account -> account.getUsername().equals(user))
+                    .findAny()
+                    .orElse(null);
+            System.out.println("Nhâp mật khẩu");
+            String pass = sc.nextLine();
+
+            if (acc.getPassword().equals(pass)) {
+                System.out.println("Chào mừng " + user);
+                System.out.println("Đăng nhập thành công");
+                service(acc);
+            } else {
+                System.out.println("1. Đăng nhập lại");
+                System.out.println("2. Quên mật khẩu");
+                int choice = sc.nextInt();
+                sc.nextLine();
+                switch (choice) {
+                    case 1:
+                        checkLogin();
+                        break;
+                    case 2:
+                        System.out.println("Nhập email để khôi phục mật khẩu");
+                        String email = sc.nextLine();
+                        if (acc.getEmail().equals(email)) {
+                            System.out.println("Nhập mật khẩu mới");
+                            String newPassword = inputPassword();
+                            acc.setPassword(newPassword);
+                            checkLogin();
+                        } else {
+                            System.out.println("Tài khoản chưa tồn tại");
+                        }
+                        break;
+                    default:
+                        System.out.println("Không có lựa chọn này");
+                }
+            }
+
+        } else {
             System.out.println("Kiểm tra lại username");
-            login();
         }
-        return user;
+
+
     }
 
-    public String checkPassword () {
-        String pass = sc.nextLine();
-        if (!listAccount.stream().anyMatch(a -> a.getPassword().equals(pass))) {
-            System.out.println("1. Đăng nhập lại");
-            System.out.println("2. Quên mật khẩu");
+    public void service(Account acc)  {
+
+        while (true) {
+            System.out.println("1 - Thay đổi username");
+            System.out.println("2 - Thay đổi email");
+            System.out.println("3 - Thay đổi mật khẩu");
+            System.out.println("4 - Đăng xuất");
+            System.out.println("0 - Thoát chương trình");
             int choice = sc.nextInt();
             sc.nextLine();
             switch (choice) {
                 case 1:
-                    login();
+                    System.out.println("Nhập username mới");
+                    String newUsername = sc.nextLine();
+                    acc.setUsername(newUsername);
+                    System.out.println("Đổi username thành công");
+                    System.out.println(acc);
+
                     break;
+
                 case 2:
-                    System.out.println("Nhập email để khôi phục mật khẩu");
-                    String email = inputEmail();
-                    if (listAccount.stream().anyMatch(a -> a.getEmail().equals(email))) {
-                          for (Account a: listAccount) {
-                            if (a.getEmail().equals(email)) {
-                            System.out.println("Nhập mật khẩu mới");
-                            String newPassword = sc.nextLine();
-                            a.setEmail(newPassword);
-                            System.out.println(a);
-                        }
-                    }
-                          login();
+                    System.out.println("Nhập email mới");
+                    String newEmail = inputEmail();
+                    if (newEmail == "") {
+                        System.out.println("Đổi email không thành công");
                     } else {
-                        System.out.println("Tài khoản chưa tồn tại");
+                        acc.setEmail(newEmail);
+                        System.out.println("Đổi email thành công");
+                        System.out.println(acc);
                     }
+                    break;
+                case 3:
+                    System.out.println("Nhập mật khẩu mới");
+                    String newPassword = inputPassword();
+                    if (newPassword == "") {
+                        System.out.println("Đổi mật khẩu không thành công");
+                    } else {
+                        acc.setPassword(newPassword);
+                        System.out.println("Đổi mật khẩu thành công");
+                        System.out.println(acc);
+                    }
+
+                    break;
+                case 4:
+                    menu();
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("Không có lựa chọn này");
             }
         }
-        return pass;
     }
+
 
     public String inputPassword()  {
+        String validatedPassword = "";
         String password = sc.nextLine();
-        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[.,-_;])(?=\\\\S+$).{7,15}$";
-        if (!Pattern.matches(regex, password)) {
-            System.out.println("\"Password phải dài từ 7 đến 15 ký tự, chứa ít nhất 1 ký tự in hoa, 1 ký tự đặc biệt (. , - _ ;)\"");
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[.,-_;]).{7,15}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        if (matcher.matches()) {
+            validatedPassword = password;
+        } else {
+            System.out.println("Mật khẩu phải dài từ 7 đến 15 ký tự, chứa ít nhất 1 ký tự in hoa, 1 ký tự đặc biệt (. , - _ ;)");
         }
-
-        return password;
+        return validatedPassword;
     }
 
-    public String inputEmail () {
+    public String inputEmail ()  {
+        String validatedEmail = "";
         String email = sc.nextLine();
-        String regex = "^(.+)@(\\\\S+)$";
-        if (!Pattern.matches(regex, email)) {
+        String regex = "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (matcher.matches()) {
+            validatedEmail = email;
+        } else {
             System.out.println("Email không hợp lệ");
+
         }
-        return email;
+
+        return validatedEmail;
     }
 
 
     public void menu()  {
-        System.out.println("1. Đăng nhập");
-        System.out.println("2. Đăng ký");
-        int choice = sc.nextInt();
-        sc.nextLine();
-        switch (choice) {
-            case 1:
-                login();
-                break;
-            case 2:
-                register();
-                break;
-
-        }
-
-    }
-
-
-    public void login() {
-        getAccount();
-        System.out.println("Nhập username");
-        String userName = checkUsername();
-        System.out.println("Nhập mật khẩu");
-        String passWord = checkPassword();
-        System.out.println("Đăng nhập thành công");
-        System.out.println("Bạn có thể thực hiên các công việc sau");
-        System.out.println("1. Thay đổi username");
-        System.out.println("2. Thay đổi email");
-        System.out.println("3. Thay đổi mật khẩu");
-        System.out.println("4. Đăng xuất ");
-        System.out.println("0. Thoát chương trình");
-        int choice = sc.nextInt();
-        sc.nextLine();
-        switch (choice) {
-            case 1:
-                for (Account a: listAccount) {
-                    if (a.getUsername().equals(userName)) {
-                        System.out.println("Nhập username mới");
-                        String newUserName = sc.nextLine();
-                        a.setUsername(newUserName);
-                        System.out.println(a);
-                    }
-                }
-
-                break;
-            case 2:
-                for (Account a: listAccount) {
-                    if (a.getUsername().equals(userName)) {
-                        System.out.println("Nhập email mới");
-                        String newEmail = sc.nextLine();
-                        a.setEmail(newEmail);
-                        System.out.println(a);
-                    }
-                }
-                break;
-            case 3:
-                for (Account a: listAccount) {
-                    if (a.getUsername().equals(userName)) {
-                        System.out.println("Nhập mật khẩu mới");
-                        String newPassword = sc.nextLine();
-                        a.setEmail(newPassword);
-                        System.out.println(a);
-                    }
-                }
-            case 4:
-                login();
-                break;
-            case 0:
-                System.exit(0);
-            default:
-                System.out.println("Không có lựa chọn này");
-
-        }
-
-
-
+       while (true) {
+           System.out.println("1. Đăng nhập");
+           System.out.println("2. Đăng ký");
+           int choice = sc.nextInt();
+           sc.nextLine();
+           switch (choice) {
+               case 1:
+                   checkLogin();
+                   break;
+               case 2:
+                   register(listAccount);
+                   break;
+               default:
+                   System.out.println("Không có lựa chọn này");
+           }
+       }
 
     }
 
-    public void register() {
-        getAccount();
+
+    public void register(ArrayList<Account> listAccount)  {
         System.out.println("Nhập username đăng ký");
         String username = sc.nextLine();
         System.out.println("Nhập mật khẩu");
         String password = inputPassword();
         System.out.println("Nhập email");
         String email = inputEmail();
-        if (listAccount.stream().anyMatch(a -> a.getUsername().equals(username))) {
+        if (listAccount.stream().anyMatch(a -> a.getEmail().equals(email))) {
             System.out.println("Email đã tồn tại");
-        } else {
-            listAccount.add(new Account(username, password, email));
         }
+
+        if (password == "" || email == "") {
+            System.out.println("Đăng ký không thành công do email hoặc mật khẩu không hợp lệ");
+        }
+        else {
+            listAccount.add(new Account(username, password, email));
+            print(listAccount);
+        }
+
     }
 
 
--- Lesson/Lesson20/src/Main.java
+++ Lesson/Lesson20/src/Main.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         AccountRepository repo = new AccountRepository();
-        repo.menu();
+            repo.menu();
+
+        }
+
 
 
     }
-}
+
